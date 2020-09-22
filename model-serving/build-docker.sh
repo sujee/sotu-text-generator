a7d900184a2cdc640998825e7781d363340c062f
Sujee Maniyam <sujee@elephantscale.com>
2020-09-22 13:02:48 -0700

    selecting models for production

--- model-serving/build-docker.sh
+++ model-serving/build-docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # sync up models & vocab
-ln -f ../models/*  models/
+rm -rf models/*
+ln -f ../models/*model-1.h5  models/
 ln -f ../tokenizer-vocabulary/*  models/
 
 docker build . -t sujee/sotu-text-generator
